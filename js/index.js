f9db9a5d0ebb4e9e58ccc35a08aa5541ea50fb64
Jason Farrell <jason@useallfive.com>
2020-07-17 11:02:28 -0700

    Remove python test from frontend test.

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import '@babel/polyfill'
-
-const init = async () => {
-    const response = await fetch('/data')
-    const responseJson = await response.json()
-    document.querySelector('h2').innerText = responseJson.site_title
-}
-init()
